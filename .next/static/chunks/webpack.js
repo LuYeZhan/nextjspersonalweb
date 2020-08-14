7392367d2c7827e8f05d57117fc77ff1c6d68fd7
Ye Zhan <lu@hooli.store>
2020-08-14 15:09:47 +0200

    fix build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "48a4393051ee99c7a6dc";
+/******/ 	var hotCurrentHash = "6d834512849733bfa048";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
